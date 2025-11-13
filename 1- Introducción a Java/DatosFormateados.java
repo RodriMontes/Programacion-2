dd233334b45fa58bbf2b766bbf69408e0ba288f6
Rodrigo Montes Sare <104746651+RodriMontes@users.noreply.github.com>
2025-11-13 19:45:30 -0300

    Create DatosFormateados.java

---
+++ 1- Introducción a Java/DatosFormateados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1_introduccionjava;
+
+/**
+ *
+ * Actividad 6
+ */
+public class DatosFormateados {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Perez\nEdad: 30 anos\nDireccion: \"Calle Falsa 123\"");
+    }
+}
+
