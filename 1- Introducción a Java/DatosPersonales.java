12dafeeef002117c933bebb1fa49289d526470eb
Rodrigo Montes Sare <104746651+RodriMontes@users.noreply.github.com>
2025-11-13 19:40:55 -0300

    Create DatosPersonales.java

---
+++ 1- Introducción a Java/DatosPersonales.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1_introduccionjava;
+
+/**
+ *
+ * @author vlc
+ */
+public class DatosPersonales {
+    public static void main(String[] args) {
+        String nombre = "Rodrigo";
+        int edad = 29;
+        double altura = 1.78;
+        boolean estudiante = true;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("¿Es estudiante?: " + estudiante);
+    }
+}
