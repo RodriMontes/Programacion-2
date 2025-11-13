ac0d875017ecd72c77b6809edff02e89725c30e3
Rodrigo Montes Sare <104746651+RodriMontes@users.noreply.github.com>
2025-11-13 19:53:06 -0300

    Create PruebaEsctritorio.java

---
+++ 1- Introducción a Java/PruebaEsctritorio.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1_introduccionjava;
+
+/**
+ *
+ * @author vlc
+ */
+public class PruebaEscritorio {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int resultado = a / b;
+        System.out.println("Resultado: " + resultado);
+    }
+}
+
+/**
+ *
+ * Paso	Instrucción	Valor de a	Valor de b	Valor de resultado
+    1	int a = 5;          5                —                  —
+    2	int b = 2;          5                2                  —
+    3	resultado = a / b;  5                2                  2
+    4	println("Resultado: "...)	5	2	2
+ */
