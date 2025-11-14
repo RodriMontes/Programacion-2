ffce0b535d7026a15327349e493cac137f363874
Rodrigo Montes Sare <104746651+RodriMontes@users.noreply.github.com>
2025-11-13 23:32:04 -0300

    Add ActualizarStock class to manage product stock

---
+++ 2- Programación Estructura/ActualizarStock.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_programacionestructurada;
+
+/**
+ *
+ * @author vlc
+ */
+import java.util.Scanner;
+
+public class ActualizarStock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese el stock actual del producto: ");
+        int stockActual = sc.nextInt();
+        System.out.print("Ingrese la cantidad vendida: ");
+        int cantidadVendida = sc.nextInt();
+        System.out.print("Ingrese la cantidad recibida: ");
+        int cantidadRecibida = sc.nextInt();
+
+        int nuevoStock = actualizarStock(stockActual, cantidadVendida, cantidadRecibida);
+
+        System.out.println("El nuevo stock del producto es: " + nuevoStock);
+
+        sc.close();
+    }
+
+    public static int actualizarStock(int stockActual, int cantidadVendida, int cantidadRecibida) {
+        return stockActual - cantidadVendida + cantidadRecibida;
+    }
+}
